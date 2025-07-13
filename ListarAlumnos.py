65ba6d5996013e16214bded0c99271cebca65b71
Dylan Andres Cabezas Ramirez <166071032+DylanCabezas@users.noreply.github.com>
2025-07-12 20:48:34 -0500

    Update ListarAlumnos.py

--- ListarAlumnos.py
+++ ListarAlumnos.py
@@ -4,11 +4,9 @@ import os
 import json
 
 def lambda_handler(event, context):
-
     secret_id = os.environ['DB_SECRET_ARN']
     database = os.environ['DB_NAME']
 
-    # Leer el secreto desde AWS Secrets Manager
     sm = boto3.client('secretsmanager')
     response = sm.get_secret_value(SecretId=secret_id)
     secret = json.loads(response['SecretString'])
@@ -17,32 +15,21 @@ def lambda_handler(event, context):
     user = secret['username']
     password = secret['password']
 
-    connection = None
+    connection = pymysql.connect(
+        host=host,
+        user=user,
+        password=password,
+        db=database,
+        connect_timeout=5
+    )
 
-    try:
-        connection = pymysql.connect(
-            host=host,
-            user=user,
-            password=password,
-            db=database,
-            connect_timeout=5
-        )
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM alumnos;")
+        results = cursor.fetchall()
 
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM alumnos;")
-            results = cursor.fetchall()
+    connection.close()
 
-        return {
-            "statusCode": 200,
-            "body": json.dumps(results, default=str)
-        }
-
-    except Exception as e:
-        return {
-            "statusCode": 500,
-            "error": str(e)
-        }
-
-    finally:
-        if connection:
-            connection.close()
+    return {
+        "statusCode": 200,
+        "body": json.dumps(results, default=str)
+    }
